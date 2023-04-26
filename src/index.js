7b2887a79fec2dc05987f0fe266d300cf57ff046
Roman-Nechaev <nrchaevroman@gmail.com>
2023-04-26 18:26:32 +0300

    fix url

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { startTimer } from './features/timer';
 import { isPrintText } from './features/printText';
 import { onFetchData } from './features/storingResponses';
 import { progressBar } from './features/progressBar';
-import { dataStor } from './features/dataStor';
+// import { dataStor } from './features/dataStor';
 
 const timerRef = document.getElementById('deadline-timer');
 const quizFormRef = document.querySelector('#quizForm');
@@ -17,9 +17,48 @@ const fetchBtnRef = document.querySelector('.finish-btn');
 const progressBarRef = document.getElementById('progressBar');
 const btnPage = document.querySelector('.btn');
 
-dataStor(quizFormRef, btnPage);
+// dataStor(quizFormRef, btnPage);
 onFetchData(fetchRenderRef, fetchBtnRef);
 isPrintText();
 
+let storingResponses = JSON.parse(localStorage.getItem('collectData')) || [];
+
 if (timerRef) startTimer();
 if (progressBarRef) progressBar();
+
+function onCollectData(item) {
+  storingResponses.push(item);
+  localStorage.setItem('collectData', JSON.stringify(storingResponses));
+}
+let count = 1;
+
+if (quizFormRef) {
+  btnPage.disabled = true;
+  quizFormRef.addEventListener('submit', onSubmitValue);
+
+  function checkValue() {
+    quizFormRef.addEventListener('change', e => {
+      e.currentTarget.item.value;
+
+      btnPage.disabled = false;
+    });
+  }
+  checkValue();
+}
+
+function onSubmitValue(evt) {
+  evt.preventDefault();
+  const currentItem = evt.currentTarget.item.value;
+
+  onCollectData(currentItem);
+
+  goToPage();
+}
+
+const goToPage = () => {
+  let currentPage = window.location.pathname.replace(/\D/g, '');
+
+  count += Number(currentPage);
+
+  window.location.href = `/quiz/${count}`;
+};
